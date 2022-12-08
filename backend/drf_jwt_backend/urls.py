30005634a9ea48d1b07bc3f5dfe65f76d45b7b82
Alex <alexandertaylor315@gmail.com>
2022-12-07 19:37:29 -0500

    created comment app and model with keys user,video_id, text, likes, and disklikes + registered comment model in admin.py + created commentserializer + created comment_list function in views.py

--- backend/drf_jwt_backend/urls.py
+++ backend/drf_jwt_backend/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/comment/', include('comment.urls')),
     path('api/auth/', include('authentication.urls')),
     path('api/cars/', include('cars.urls')),
 ]
